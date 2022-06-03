29f8225df5ccb24ddebb1b8be4bd493194ec37cc
enweazudaniel <daniel.enweazu@student.aul.edu.ng>
2022-06-03 13:47:35 +0100

    Add List of Fav Food

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>Hello World!</h1>
-    <p>Doc</p>
+    <h1>My Favorite Food</h1>
+    <p>
+      <ul>
+        <li>Spag</li>
+        <li>Momos</li>
+        <li>Fried Rice</li>
+      </ul>
+    </p>
   </div>,
   document.getElementById("root")
 );
