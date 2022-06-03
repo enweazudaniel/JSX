4dab50b6bf35eb7cac354d80ab27cf238aec0770
enweazudaniel <daniel.enweazu@student.aul.edu.ng>
2022-06-03 15:09:51 +0100

    Javascript exp

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 const name = "hetti";
+const fName = "Enweazu";
+const lName = "Daniel";
+const copyright = "Copyright 2022";
 ReactDOM.render(
   <div>
     <h1>My Favorite Food</h1>
     <p>
+      {fName} {lName} {}
       Your lucky number {Math.floor(Math.random() * 10)}
       <ul>
         <li>Spag{name}</li>
         <li>Momos</li>
         <li>Fried Rice</li>
       </ul>
+      {copyright}
     </p>
   </div>,
   document.getElementById("root")
