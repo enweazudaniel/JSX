185eb4b26dcd2adeca5956dd45760946acb8cdac
enweazudaniel <daniel.enweazu@student.aul.edu.ng>
2022-06-06 17:57:34 +0100

    Time change

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 const name = "hetti";
 const fName = "Enweazu";
 const lName = "Daniel";
 const d = new Date();
+const currentTime = d.getHours();
 const year = d.getFullYear();
+
+let greeting;
+
+const customStyle = {
+  color: ""
+};
+
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Night";
+  customStyle.color = "blue";
+}
+
 ReactDOM.render(
   <div>
-    <h1 className="heading">My Favorite Food</h1>
+    <h1 className="heading" style={customStyle}>
+      {greeting}
+    </h1>
+    <h1 className="heading">Here is my Favorite Food</h1>
     <img
       className="food-img"
       alt="spaghetti"
