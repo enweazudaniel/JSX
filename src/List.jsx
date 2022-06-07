f2396f0337dc580702f5dcb5ab84e6f1552f2504
enweazudaniel <daniel.enweazu@student.aul.edu.ng>
2022-06-07 12:57:53 +0100

    Add react components

---
+++ src/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function List() {
+  return (
+    <img
+      className="food-img"
+      alt="spaghetti"
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU1eJylAOoW16FYeF_t5-xneKlemWuPEAHGFyF9o_G61vb-hJwhu2qJLPMaMVnjod9ZVg&usqp=CAU"
+    />
+  );
+}
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Heading from "./Heading";
+import List from "./List";
 
 const name = "hetti";
 const fName = "Enweazu";
@@ -30,12 +32,8 @@ ReactDOM.render(
     <h1 className="heading" style={customStyle}>
       {greeting}
     </h1>
-    <h1 className="heading">Here is my Favorite Food</h1>
-    <img
-      className="food-img"
-      alt="spaghetti"
-      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU1eJylAOoW16FYeF_t5-xneKlemWuPEAHGFyF9o_G61vb-hJwhu2qJLPMaMVnjod9ZVg&usqp=CAU"
-    />
+    <Heading />
+    <List />
   </div>,
   document.getElementById("root")
 );
