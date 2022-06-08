ccc819d0660fd913cfd31ed3ea875fbbc76c1cf7
enweazudaniel <daniel.enweazu@student.aul.edu.ng>
2022-06-08 15:55:07 +0100

    import module

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,6 +1,29 @@
 import React from "react";
 
 function Heading() {
+  const name = "hetti";
+  const fName = "Enweazu";
+  const lName = "Daniel";
+  const d = new Date();
+  const currentTime = d.getHours();
+  const year = d.getFullYear();
+
+  let greeting;
+
+  const customStyle = {
+    color: ""
+  };
+
+  if (currentTime < 12) {
+    greeting = "Good Morning";
+    customStyle.color = "red";
+  } else if (currentTime < 18) {
+    greeting = "Good Afternoon";
+    customStyle.color = "green";
+  } else {
+    greeting = "Good Night";
+    customStyle.color = "blue";
+  }
   return <h1 className="heading">Here is my Favorite Food</h1>;
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { add, divide, multiply, subtract } from "./cal";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ul>
+    <li>{add(1, 2)}</li>
+    <li>{subtract(1, 2)}</li>
+    <li>{multiply(1, 2)}</li>
+    <li>{divide(1, 2)}</li>
+  </ul>,
+
+  document.getElementById("root")
+);
